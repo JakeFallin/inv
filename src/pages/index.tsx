060f152fe1241eefa09559910935f5498c4d36e9
jakefallin <jakefallin@gmail.com>
2023-06-27 11:08:34 -0400

    integrated prism and axiom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const {data} = api.posts.getAll.useQuery();
   const user = useUser();
+
   return (
     <>
       <Head>
@@ -17,6 +18,11 @@ export default function Home() {
       <div>
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton/> }   
+      </div>
+      <div>
+        {data?.map((post) => (
+          <div key={post.id}>{post.content}</div>
+        ))}
       </div>
       </main>
     </>
